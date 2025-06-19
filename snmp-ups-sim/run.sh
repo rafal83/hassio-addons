8b955d6e381575bd76125aae7f4184f55a9dfd37
raphael <raphael.dauchy@kwote.fr>
2025-06-19 13:51:36 +0200

    fix run.sh

--- snmp-ups-sim/run.sh
+++ snmp-ups-sim/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# ⬅️ Sourcer bashio (important)
+source /usr/bin/bashio
+
 export MQTT_BROKER="$(bashio::config 'mqtt_broker')"
 export MQTT_USERNAME="$(bashio::config 'mqtt_username')"
 export MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
