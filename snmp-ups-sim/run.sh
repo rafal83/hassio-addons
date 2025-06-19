73496796bccfa2ee0d8f051a99bb19da57247e98
raphael <raphael.dauchy@kwote.fr>
2025-06-19 14:51:26 +0200

    fix

--- snmp-ups-sim/run.sh
+++ snmp-ups-sim/run.sh
@@ -3,9 +3,14 @@ set -e
 
 # Lire les options depuis /data/options.json (fichier injecté par HA)
 MQTT_BROKER=$(jq -r '.mqtt_broker' /data/options.json)
+MQTT_PORT=$(jq -r '.mqtt_port' /data/options.json)
 MQTT_USERNAME=$(jq -r '.mqtt_username' /data/options.json)
 MQTT_PASSWORD=$(jq -r '.mqtt_password' /data/options.json)
-MQTT_PORT=$(jq -r '.mqtt_password' /data/options.json)
+
+export MQTT_BROKER
+export MQTT_PORT
+export MQTT_USERNAME
+export MQTT_PASSWORD
 
 # Utilise les variables d'environnement standards
 echo "🔧 MQTT_BROKER: $MQTT_BROKER"
