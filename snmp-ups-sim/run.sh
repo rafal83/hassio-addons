fc6b46267905aafbbacf0dcab787ca1c52353314
raphael <raphael.dauchy@kwote.fr>
2025-06-19 14:16:45 +0200

    try

--- snmp-ups-sim/run.sh
+++ snmp-ups-sim/run.sh
@@ -1,11 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
 
-source /usr/lib/bashio/bashio.sh
-
-export MQTT_BROKER="$(bashio::config 'mqtt_broker')"
-export MQTT_USERNAME="$(bashio::config 'mqtt_username')"
-export MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
-
+# Utilise les variables d'environnement standards
 echo "🔧 MQTT_BROKER: $MQTT_BROKER"
+echo "🔧 MQTT_USERNAME: $MQTT_USERNAME"
+
 node snmp-agent.js
